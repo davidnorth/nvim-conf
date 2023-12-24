da336d8ea6cf8ca60418330a4b022407fb9eac03
David North <davidnorth@gmail.com>
2023-12-24 18:18:39 +0000

    disable pair matching. pope style surround mappings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,4 +9,14 @@ vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
 })
 vim.g.copilot_no_tab_map = true
 
+-- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Use only system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Tim Pope style surround beavior
+-- Remap adding surrounding to Visual mode selection
+vim.api.nvim_set_keymap("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+-- Make special mapping for "add surrounding for line"
+vim.api.nvim_set_keymap("n", "yss", "ys_", { noremap = false })
---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,24 @@
+-- tip pope style surround
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      custom_surroundings = {
+        ["("] = { output = { left = "( ", right = " )" } },
+        ["["] = { output = { left = "[ ", right = " ]" } },
+        ["{"] = { output = { left = "{ ", right = " }" } },
+        ["<"] = { output = { left = "< ", right = " >" } },
+      },
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+        update_n_lines = "",
+      },
+      search_method = "cover_or_next",
+    },
+  },
+}
