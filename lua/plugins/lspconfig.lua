aa5fdbeff7a62bb95359fb2639d04e5c5ccc0a95
David North <davidnorth@gmail.com>
2025-01-23 15:39:41 +0000

    No type annotations etc. update snacks config with something or other

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+
+      inlay_hints = {
+        enabled = false,
+      },
     },
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
-      file_ignore_patterns = { "tmp", "node_modules", "%.git/", "dist/", "%.lock", "__pycache__" },
+      file_ignore_patterns = { "^./log", "^log/", "tmp", "node_modules", "%.git/", "dist/", "%.lock", "__pycache__" },
     },
   },
 }
